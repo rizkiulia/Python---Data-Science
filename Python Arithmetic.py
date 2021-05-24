5dfa83041f0eccfff61399d573b013f12a439d73
rizkiulia <rizkiulia96@gmail.com>
2021-05-24 14:28:56 +0700

    Create Python Arithmetic.py

---
+++ Python Arithmetic.py
@@ -0,0 +1,22 @@
+a = int(input("Inputkan nilai a: "))
+b = int(input("Inputkan nilai b: "))
+
+#Contoh statement pertambahan antara 2 number di Python
+# Operator Penjumlahan
+c = a + b
+print ("Hasil %i + %i = %i" % (a,b,c))
+
+#Contoh statement perkurangan antara 2 number di Python
+# Operator Pengurangan
+c = a - b
+print (f'Hasil {a} - {b} = {c}')
+
+#Contoh statement perkalian antara 2 number di Python
+# Operator Perkalian
+c = a * b
+print ("Hasil %d * %d = %d" % (a,b,c))
+
+#Contoh statement pembagian antara 2 number di Python
+# Operator Pembagian
+c = a / b
+print ("Hasil %d / %d = %d" % (a,b,c))
