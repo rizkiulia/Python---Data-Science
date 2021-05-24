ea87d1ee5632a010d94d122f918aefbfcd638ab3
rizkiulia <rizkiulia96@gmail.com>
2021-05-24 14:31:48 +0700

    Create Assign Variable dan Tipe Data Integer, Float.py

---
+++ Assign Variable dan Tipe Data Integer, Float.py
@@ -0,0 +1,13 @@
+#Buat suatu variabel a dan b, dimana a dan b adalah nilai bertipe data numeric
+#Berikan suatu nilai bertipe data integer, hasil pembagian dari a dengan b
+#Berikan suatu nilai bertipe data float, hasil pembagian dari a dengan b
+
+
+a = 15
+b = 6
+
+c = a // b
+print (c)
+
+c = a / b
+print (c)
